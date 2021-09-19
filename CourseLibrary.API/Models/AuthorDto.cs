33ee56b858f05d272b40937b2a3a39fad128a6f0
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-09-19 11:55:30 -0700

    Creating DOB helper function

--- CourseLibrary.API/Models/AuthorDto.cs
+++ CourseLibrary.API/Models/AuthorDto.cs
@@ -8,9 +8,8 @@ namespace CourseLibrary.API.Models
     public class AuthorDto
     {
         public Guid Id { get; set; }
-        public string FirstName { get; set; }
-        public string LastName { get; set; }
-        public DateTimeOffset DateOfBirth { get; set; }
+        public string Name { get; set; }
+        public int Age { get; set; }
         public string MainCategory { get; set; }
     }
 }
