7d4bc62e9a0396984be389319b42011589323e46
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-10-10 15:42:55 -0700

    Class level input validation with custom attribute

---
+++ CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentThanDescriptionAttribute.cs
@@ -0,0 +1,25 @@
+﻿using CourseLibrary.API.Models;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CourseLibrary.API.ValidationAttributes
+{
+    public class CourseTitleMustBeDifferentThanDescriptionAttribute : ValidationAttribute
+    {
+        protected override ValidationResult IsValid(object value, ValidationContext validationContext)
+        {
+            var coursce = (CourseForCreationDto)validationContext.ObjectInstance;
+
+            if (coursce.Title == coursce.Description)
+            {
+                return new ValidationResult(
+                    "The provided description should be different from the title",
+                    new[] { nameof(CourseForCreationDto) });
+            }
+            return ValidationResult.Success;
+        }
+    }
+}
