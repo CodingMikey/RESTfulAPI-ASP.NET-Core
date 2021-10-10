c1fdc070c9a97cfc7a68a91c264352c593f18530
Mikhail Galas <39977309+CodingMikey@users.noreply.github.com>
2021-10-10 15:55:06 -0700

    Adding custom error messages to CourseForCreationDto

--- CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescription.cs
+++ CourseLibrary.API/ValidationAttributes/CourseTitleMustBeDifferentFromDescription.cs
@@ -15,8 +15,7 @@ namespace CourseLibrary.API.ValidationAttributes
 
             if (coursce.Title == coursce.Description)
             {
-                return new ValidationResult(
-                    "The provided description should be different from the title",
+                return new ValidationResult(ErrorMessage,
                     new[] { nameof(CourseForCreationDto) });
             }
             return ValidationResult.Success;
